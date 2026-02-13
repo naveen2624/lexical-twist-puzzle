fa0c4fed9b20db4b811f9abad502f06654922713
Naveen <naveenudai26@gmail.com>
2026-02-13 17:20:28 +0530

    [Naveen]: [UC2]: [Add]: Accepted two words from the user for puzzle processing.

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class LexicalTwistPuzzle {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Lexical Twist Puzzle");
+        System.out.println("Enter the first word");
+        String word1=sc.nextLine();
+        System.out.println("Enter the second word");
+        String word2=sc.nextLine();
     }
 }
